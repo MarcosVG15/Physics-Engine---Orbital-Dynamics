bb7ddcb85ba6e1dad6742b6d73f6c456dcea28b1
MarcosVG15 <m.vargasgarcia@student.maastrichtuniversity.nl>
2025-05-26 19:18:59 +0200

    RK4 Physics Engine functional

---
+++ src/Physics_Engine/AttemptSolarSystem/Interfaces/function.java
@@ -0,0 +1,11 @@
+package src.Physics_Engine.AttemptSolarSystem.Interfaces;
+
+
+import src.Physics_Engine.AttemptSolarSystem.AstralObject;
+
+import java.util.ArrayList;
+
+public interface function {
+    public vectorInterface computeDerivative(int planet, vectorInterface Vector,ArrayList<AstralObject> solarSystem) ;
+
+}
---
+++ src/Physics_Engine/AttemptSolarSystem/VelocityFunction.java
@@ -0,0 +1,15 @@
+package src.Physics_Engine.AttemptSolarSystem;
+
+
+import src.Physics_Engine.AttemptSolarSystem.Interfaces.function;
+import src.Physics_Engine.AttemptSolarSystem.Interfaces.vectorInterface;
+
+import java.util.ArrayList;
+
+public class VelocityFunction implements function {
+    @Override
+    public vectorInterface computeDerivative(int planet, vectorInterface Vector, ArrayList<AstralObject> solarSystem) {
+
+        return Vector;
+    }
+}
